49ebf18b315efd9f38b5d0020329eef1307551e1
Elaine C <laineypls@gmail.com>
2019-06-03 04:29:15 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,14 +13,14 @@ end
 def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
     true
+  else
+    false
   end
 end
 
 def valid_move?(board, index)
   if position_taken?(board, index) == false && index.between?(0, 8)
     true
-  elsif
-    false
   end
 end
 
